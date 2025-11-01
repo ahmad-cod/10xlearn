e6d4615b3055398740fe4c0afa4810618b9322d5
ahmad-cod <ahmadadewale800@gmail.com>
2025-11-01 01:00:48 +0100

    feat: add work in progress message

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -15,6 +15,7 @@ export default async function ProtectedPage() {
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
       <div className="w-full">
+        <h2 className="p-4 text-lg text-primary">Work in Progress...</h2>
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
           This is a protected page that you can only see as an authenticated
