03acd1f59dcdc8558fe8812e5588f70c09badc9d
ahmad-cod <ahmadadewale800@gmail.com>
2025-11-07 23:12:14 +0100

    refactor: Improve layout structure for username input in chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { RealtimeChat } from "@/components/realtime-chat";
 import { useState } from "react";
-// import { useMessagesQuery }
 
 export default function ChatPage() {
   const [username, setUsername] = useState<string>("");
@@ -9,21 +8,23 @@ export default function ChatPage() {
 
   if (!isJoined) {
     return (
-      <div className="p-4">
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter your username"
-          className="border p-2 rounded"
-        />
-        <button
-          onClick={() => setIsJoined(true)}
-          disabled={!username}
-          className="ml-2 px-4 py-2 bg-primary text-white rounded disabled:bg-gray-300"
-        >
-          Join Chat
-        </button>
+      <div className="grid place-content-center">
+        <div className="p-4">
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter your username"
+            className="border p-2 rounded"
+          />
+          <button
+            onClick={() => setIsJoined(true)}
+            disabled={!username}
+            className="mt-2 sm:mt-0 sm:ml-2 px-4 py-2 bg-primary text-white rounded disabled:bg-gray-300"
+          >
+            Join Chat
+          </button>
+        </div>
       </div>
     );
   }
